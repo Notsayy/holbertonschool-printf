6366c39f3f4aaceaa12a22d41f53086703050c1c
Notsayy <jtertre25@gmail.com>
2024-11-26 13:55:51 +0100

    Add the file print_percent.c

---
+++ print_percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_percent - Prints the percent sign '%'
+ * @args: A list of arguments
+ *
+ * Return: Nothing
+ */
+
+void print_percent(va_list args)
+{
+	(void)args;
+	_putchar('%');
+}
