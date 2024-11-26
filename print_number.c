b64fde31d2fca4cd8202ccfbc10fa120b71ab9d5
Notsayy <jtertre25@gmail.com>
2024-11-26 16:12:14 +0100

    FIX: I changed the function to int and enhanced it to count the number of characters that will be printed.

--- print_number.c
+++ print_number.c
@@ -3,12 +3,15 @@
  * print_number - Recursively prints an integer
  * @value: The integer to print
  *
- * Return: Nothing
+ * Return: The total number of digits printed.
  */
-void print_number(int value)
+int print_number(unsigned int value)
 {
+	int count = 0;
+
 	if (value / 10)
-		print_number(value / 10);
+		count += print_number(value / 10);
 
 	_putchar((value % 10) + '0');
+	return (count + 1);
 }
