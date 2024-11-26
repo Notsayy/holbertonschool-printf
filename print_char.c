af2e7615f02e9411fd8a0cba7456b099c77d182d
Notsayy <jtertre25@gmail.com>
2024-11-26 16:45:53 +0100

    FIX: I changed the function to int and enhanced it to count the number of characters that will be printed.

--- print_char.c
+++ print_char.c
@@ -4,10 +4,11 @@
  * print_char - Prints a character.
  * @args: A list of arguments containing the character to print.
  *
- * Return: Nothing
+ * Return: The total number of digits printed.
  */
 
-void print_char(va_list args)
+int print_char(va_list args)
 {
 	_putchar(va_arg(args, int));
+	return (1);
 }
