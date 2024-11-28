4d980deab6e1885a03b383ca67b1819833beb707
« Ephely » <ephely1@gmail.com>
2024-11-28 09:43:30 +0100

    Update print_string

--- print_string.c
+++ print_string.c
@@ -1,22 +1,21 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_string - Prints a string.
  * @args: A list of arguments containing the string to print.
  *
- * Return: Nothing
+ * Return:The number of characters printed.
  */
-
-void print_string(va_list args)
+int print_string(va_list args)
 {
-	char *string = va_arg(args, char *);
-
-	if (string == NULL)
-		string = "(nil)";
-
-	while (*string)
-	{
-		_putchar(*string++);
-	}
+    char *string = va_arg(args, char *);
+    int count = 0;
+    if (string == NULL || *string == '\0')
+        return (0);
+    while (*string)
+    {
+        _putchar(*string++);
+        count++;
+    }
+    return (count);
 }
