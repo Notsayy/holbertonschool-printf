8797832f63a93305f5fc17c66f253749e52f11cc
Notsayy <jtertre25@gmail.com>
2024-11-28 11:21:55 +0100

    FIX: The function works now if just one % is printed

--- _printf.c
+++ _printf.c
@@ -7,13 +7,10 @@
  * @format: A list of arguments passed to the function.
  * Return: The total number of characters printed.
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
-	unsigned int format_count = 0;
-	unsigned int length = 0;
-	unsigned int specifier_count;
+	unsigned int format_count = 0, length = 0, specifier_count;
 	int (*print_funcs[])(va_list) = {
 		print_char, print_string, print_decimal, print_integer, print_percent};
 	char format_specifiers[] = {'c', 's', 'd', 'i', '%'};
@@ -30,22 +27,24 @@ int _printf(const char *format, ...)
 			{
 				_putchar('%');
 				length++;
-				break;
-			}
-			for (specifier_count = 0; specifier_count < sizeof(format_specifiers)
-			/ sizeof(format_specifiers[0]); specifier_count++) {
+				break; }
+			for (specifier_count = 0; specifier_count < 5; specifier_count++)
+			{
 				if (format[format_count] == format_specifiers[specifier_count])
 				{
 					length += print_funcs[specifier_count](args);
-					break;
-				}
+					break; }
 			}
+			if (specifier_count == 5)
+			{
+				_putchar('%');
+				_putchar(format[format_count]);
+				length += 2; }
 		}
 		else
 		{
 			_putchar(format[format_count]);
-			length++;
-		}
+			length++; }
 		format_count++;
 	}
 	va_end(args);
