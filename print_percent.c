27b95747c1279a9608431698c2e799f05017d3cf
Notsayy <jtertre25@gmail.com>
2024-11-28 22:49:16 +0100

    Add print_percent.c: Implements functionality to print the percent sign (%), necessary for correctly formatting output in _printf function.

---
+++ print_percent.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_percent - Prints the percent sign '%'
+ * @args: A list of arguments
+ *
+ * Return: The total number of digits printed.
+ */
+
+int print_percent(va_list args)
+{
+	(void)args;
+	_putchar('%');
+	return (1);
+}
