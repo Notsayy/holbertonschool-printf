2bf74af504de06ca9ca996f2ad3f9cf25b649b51
« Ephely » <ephely1@gmail.com>
2024-11-28 09:42:44 +0100

    Update print_char

--- print_char.c
+++ print_char.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
  * print_char - Prints a character.
  * @args: A list of arguments containing the character to print.
  *
- * Return: Nothing
+ * Return: The total number of digits printed.
  */
-
-void print_char(va_list args)
+int print_char(va_list args)
 {
-	_putchar(va_arg(args, int));
+    _putchar(va_arg(args, int));
+    return (1);
 }
